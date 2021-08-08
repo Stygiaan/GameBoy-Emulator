8daeaff8287e16c2d65c387bab77ccb32c2b9284
Avid Fredrich <applemus4free@gmail.com>
2021-08-08 19:09:52 +0200

    moved objects to heap and run new console debugger

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
+#include "Debug.h"
 #include "FileLogger.h"
 #include "System.h"
 
 int main()
 {
-	FileLogger logger;
-	System system(&logger);
+	FileLogger* logger = new FileLogger();
+	System* system = new System(logger);
+
+	unsigned char breakpoints[] = { 0x00, 0x15 };
+	Debug* debug = new Debug(system);
 
 	/*std::stringstream ss;
 	ss << "Unknown Opcode: " << std::hex << 0xDD;
 
 	logger.Log(ss.str());*/
 
-	system.LoadRom("./Games/tetris.gb");
+	system->LoadRom("./Games/tetris.gb");
 
-	while (true)
+	while (system->IsRunning())
 	{
-		if (system.EmulateCycle() != 0)
-			return 0;
+		system->EmulateCycle();
+
+		debug->Step();
 	}
+
+	delete logger;
+	delete system;
+	delete debug;
 }
