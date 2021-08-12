cbd9089c1b27f3ca8bb3d1217b52fa17331bb7b7
Avid Fredrich <applemus4free@gmail.com>
2021-08-12 21:31:04 +0200

    rendering, input, library includes

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 #include "Debug.h"
+#include "Input.h"
 #include "FileLogger.h"
 #include "System.h"
+#include "Renderer.h"
 
-int main()
+int main(int argc, char** argv)
 {
 	FileLogger* logger = new FileLogger();
 	System* system = new System(logger);
 
-	unsigned char breakpoints[] = { 0x00, 0x15 };
 	Debug* debug = new Debug(system);
+	Input* input = new Input(system, logger);
+	Renderer* renderer = new Renderer(system, logger);
 
-	/*std::stringstream ss;
-	ss << "Unknown Opcode: " << std::hex << 0xDD;
-
-	logger.Log(ss.str());*/
+	logger->Log(LOG_INFO, "Test", "Test");
 
 	system->LoadRom("./Games/tetris.gb");
 
 	while (system->IsRunning())
 	{
+		// debug->Step();
+
+		input->UpdateKeymap();
+
 		system->EmulateCycle();
 
-		debug->Step();
+		renderer->Update();
 	}
 
 	delete logger;
 	delete system;
 	delete debug;
+	delete input;
+	delete renderer;
+
+	return 0;
 }
